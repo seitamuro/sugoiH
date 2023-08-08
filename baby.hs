7482035c1408bbb88011bc62bc907f2f2454a860
seitamuro <seitamuro0210@gmail.com>
2023-08-09 08:57:31 +0900

    条件を満たす三角形を取得 ~p22

--- baby.hs
+++ baby.hs
@@ -5,3 +5,7 @@ doubleSmallNumber x = if x > 100
                         else x * 2
 length' xs = sum [1 | _ <- xs]
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+rightTriangle (a, b, c) = if a*a+b*b == c || b*b+c*c==a*a || c*c+a*a==b*b then True else False
+rightTriangles xs = [x | x <- xs, rightTriangle x]
+sumIs24 (a, b, c) = a+b+c == 24
+requiredTriangle = [(a, b, c) | a <- [1..10], b <- [1..a], c <- [1..b], sumIs24 (a, b, c), rightTriangle (a, b, c)]
