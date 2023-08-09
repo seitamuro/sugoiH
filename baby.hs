717c34e58224d79ca54cc102fd7c36c48e116f08
seitamuro <seitamuro0210@gmail.com>
2023-08-09 14:18:06 +0900

    型 ~p34

--- baby.hs
+++ baby.hs
@@ -1,3 +1,4 @@
+-- 基本的な関数定義やリスト、タプルの扱い方
 doubleMe x = x + x
 doubleUs x y = x * 2 + y * 2
 doubleSmallNumber x = if x > 100
@@ -9,3 +10,16 @@ rightTriangle (a, b, c) = if a*a+b*b == c || b*b+c*c==a*a || c*c+a*a==b*b then T
 rightTriangles xs = [x | x <- xs, rightTriangle x]
 sumIs24 (a, b, c) = a+b+c == 24
 requiredTriangle = [(a, b, c) | a <- [1..10], b <- [1..a], c <- [1..b], sumIs24 (a, b, c), rightTriangle (a, b, c)]
+
+-- 型
+addThree :: Int -> Int -> Int -> Int
+addThree a b c = a + b + c
+
+-- 型クラス
+allSame :: (Eq a) => [a] -> Bool
+allSame xs = length [x | x <- xs, x == head xs] == length xs
+
+
+-- 型注釈
+readStringAsInt = read "5" :: Int
+readStringAsListInt = read "[5, 4, 3, 2, 1]" :: [Int]
